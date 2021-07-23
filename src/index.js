dfc7f6ebe8b927fd3056e5c03886ac52bcbbee83
Luis150228 <luis.rangeldiaz@gmail.com>
2021-07-23 10:53:39 -0500

    Se crean clases y metodod para anexar familires

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+import {Persona, ArbolFamiliar} from './classes';
+
+const aitaiteki = new ArbolFamiliar();
+
+const individuo = new Persona(
+	'Fernando Rangel Mendoza',
+	70,
+	'CDMX',
+	'Masculino',
+	'Abuelo',
+	'Gto'
+);
+
+aitaiteki.nuevoFamiliar(individuo);
+
+console.log(aitaiteki);
